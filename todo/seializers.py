f1306b6d57837914736ae9820697e5a5b9ec5907
Danila <kalganovdanila@gmail.com>
2022-03-24 17:37:29 +0300

    update

---
+++ todo/seializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Project, ToDo
+from authors.models import Author
+from authors.serializers import AuthorModelsSerializer
+
+
+class ProjectSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=32)
+    link_GIT = serializers.CharField(max_length=64)
+    users = AuthorModelsSerializer(many=True)
+
+
+class ToDoSerializer(serializers.Serializer):
+    project = ProjectSerializer()
+    text = serializers.CharField(max_length=256)
+    create_data = serializers.DateTimeField()
+    update_data = serializers.DateTimeField()
+    user = AuthorModelsSerializer()
+    is_active = serializers.BooleanField(default=True)
