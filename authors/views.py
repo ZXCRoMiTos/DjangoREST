11a19c52bacdc9a52dfcedf5ed486b0403674cab
Danila <kalganovdanila@gmail.com>
2022-03-29 16:05:17 +0300

    Сделал класс для постраничного вывода.

--- authors/views.py
+++ authors/views.py
@@ -1,8 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Author
 from .serializers import AuthorModelsSerializer
+from rest_framework.pagination import PageNumberPagination
+
+
+class StandardPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 
 class AuthorModelViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorModelsSerializer
+    pagination_class = StandardPagination
--- todo/views.py
+++ todo/views.py
@@ -1,16 +1,25 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Project, ToDo
 from .seializers import ProjectSerializer, ToDoSerializer
+from rest_framework.pagination import PageNumberPagination
+
+
+class StandardPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    pagination_class = StandardPagination
 
 
 class ToDoModelViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
+    pagination_class = StandardPagination
 
 
 
