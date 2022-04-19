3049ac337cbe7980514e6368ba4ee1a760050e69
Danila <kalganovdanila@gmail.com>
2022-04-19 22:41:03 +0300

    Все задания

--- geektodo/urls.py
+++ geektodo/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from authors.views import AuthorView
 from todo.views import ProjectModelViewSet, ToDoModelViewSet
+from rest_framework.authtoken import views
 
 
 router = DefaultRouter()
@@ -15,4 +16,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token)
 ]
